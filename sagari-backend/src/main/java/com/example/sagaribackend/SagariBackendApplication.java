5344c4d655e47343c28d619d2f2499754623dfb9
Sagari-afk <lergo2004@gmail.com>
2025-06-15 15:51:21 +0200

    [NEW] added initial packages for backend(spring boot) and frontend(next.js)

---
+++ sagari-backend/src/main/java/com/example/sagaribackend/SagariBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.sagaribackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagariBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SagariBackendApplication.class, args);
+    }
+
+}
